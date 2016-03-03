c8c4d30d07d18060ba7c2d5ac4ef68f989d3678e
sonia <soniasahneynagar@gmail.com>
2016-03-02 18:36:45 -0600

    fix auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
-	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+	#comment out authentication
+	#http_basic_authenticate_with name: "sonia", password: "secret", except: [:index, :show]
 	
 	def index
 		@articles = Article.all
