ee2ea30abcc10609e27f9dd602698dce291279be
sonia <soniasahneynagar@gmail.com>
2016-03-02 19:30:11 -0600

    fixed so it works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,5 @@
 class CommentsController < ApplicationController
-
-#comment out authentication
-#http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-
+  
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -15,7 +12,7 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
-
+ 
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
